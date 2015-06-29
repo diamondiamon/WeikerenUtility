f35edf6273105e1c209a0c88c97fefd7fecb403d
yiweiinfo <weikeren@hotmail.com>
2015-06-30 01:16:51 +0800

    同步代码20150630

--- Weikeren.Utility.LogWeb/App_Start/FilterConfig.cs
+++ Weikeren.Utility.LogWeb/App_Start/FilterConfig.cs
@@ -7,7 +7,6 @@ namespace Weikeren.Utility.LogWeb
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-
             filters.Add(new HandleErrorAttribute());
         }
     }

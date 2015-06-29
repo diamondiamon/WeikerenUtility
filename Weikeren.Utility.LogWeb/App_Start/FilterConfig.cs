a80b187db43ade1395bcb5dd16be445ff4db625d
kingman <diamondiamon@163.com>
2015-06-30 01:12:50 +0800

    https://git01.codeplex.com/weikerenutility

--- Weikeren.Utility.LogWeb/App_Start/FilterConfig.cs
+++ Weikeren.Utility.LogWeb/App_Start/FilterConfig.cs
@@ -7,6 +7,7 @@ namespace Weikeren.Utility.LogWeb
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+
             filters.Add(new HandleErrorAttribute());
         }
     }

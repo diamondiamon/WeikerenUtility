4ee1fb2dfe7afe1387a716bbb17d053fd63908d0
yiweiinfo <weikeren@hotmail.com>
2015-06-30 01:42:35 +0800

    test

--- Weikeren.Utility.LogWeb/App_Start/FilterConfig.cs
+++ Weikeren.Utility.LogWeb/App_Start/FilterConfig.cs
@@ -5,6 +5,10 @@ namespace Weikeren.Utility.LogWeb
 {
     public class FilterConfig
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filters"></param>
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());

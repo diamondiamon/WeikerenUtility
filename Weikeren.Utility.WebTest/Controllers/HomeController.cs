f0e2bcffa792adb53ae57b52e5c904a26970ffa3
diamondiamon <diamondiamon@163.com>
2015-09-20 22:21:25 +0800

    test

--- Weikeren.Utility.WebTest/Controllers/HomeController.cs
+++ Weikeren.Utility.WebTest/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace Weikeren.Utility.WebTest.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "99sModify this template to jump-start your ASP.NET MVC application.";
             var dt = DateTime.MinValue;
             return View();
         }

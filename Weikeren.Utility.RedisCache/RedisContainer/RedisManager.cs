3e6d3de1e3c9854a13cb4bc6674670653fa280df
diamondiamon <diamondiamon@163.com>
2016-02-16 17:09:13 +0800

    Redis

---
+++ Weikeren.Utility.RedisCache/RedisContainer/RedisManager.cs
@@ -0,0 +1,108 @@
+﻿using ServiceStack.Redis;
+using System;
+
+namespace Weikeren.Utility.RedisCache
+{
+    //public delegate void InitCompleted(RedisClient client);
+
+    /// <summary>
+    /// MemCache管理操作类
+    /// </summary>
+    public sealed class RedisManager
+    {
+        private RedisClient _client = null;
+        //private RedisEndpoint config = null;
+        //public Action<RedisClient> InitCompleted;
+
+
+        #region 单例
+        private static readonly RedisManager _instance = new RedisManager();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static RedisManager Instance
+        {
+            get { return _instance; }
+        }
+
+        private RedisManager()
+        {
+            //_endPoint = RedisCachedConfig.GetConfig();
+            //init();
+        }
+        #endregion
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void Init(string configfilepath)
+        {
+            try
+            {
+                var config = (RedisEndpoint)SerializationHelper.Load(typeof(RedisEndpoint), configfilepath);
+                _client = new RedisClient(config);
+                System.Console.WriteLine("－－－－－－－－－－－－－－－Redis初始化成功－－－－－－－－－－－－－－－");
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("－－－－－－－－－－－－－－－Redis初始化失败－－－－－－－－－－－－－－－");
+                System.Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+
+        ///// <summary>
+        ///// 初始化
+        ///// </summary>
+        ///// <param name="host">主机名</param>
+        ///// <param name="port">端口号</param>
+        ///// <param name="password">密码</param>
+        ///// <param name="db">数据库</param>
+        //public void Init(string host, int port=6379,string password=null, long db = 0)
+        //{
+        //    try
+        //    {
+        //        _client = new RedisClient(host, port, password, db);
+        //        System.Console.WriteLine("Redis初始化成功");
+        //        if(InitCompleted!=null)
+        //        {
+        //            InitCompleted.Invoke(_client);
+        //        }
+
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        System.Console.WriteLine(ex.Message);
+        //        throw ex;
+        //    }
+        //}
+
+
+
+        /// <summary>
+        /// 客户端缓存操作对象
+        /// </summary>
+        public RedisClient CacheClient
+        {
+            get
+            {
+                return _client;
+            }
+        }
+
+        /// <summary>
+        /// 关闭
+        /// </summary>
+        public void Close()
+        {
+            if (_client != null)
+            {
+                //_client.Shutdown();
+                _client.Dispose();
+                _client = null;
+            }
+        }
+
+    }
+}
---
+++ Weikeren.Utility.RedisCache/RedisContainer/RedisStrategy.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Web;
+
+namespace Weikeren.Utility.RedisCache
+{
+    /// <summary>
+    /// Redis缓存策略
+    /// </summary>
+    public class RedisStrategy : IRedisCacheStrategy
+    {
+        #region ICacheStrategy 成员
+
+        /// <summary>
+        /// 添加数据到缓存
+        /// </summary>
+        /// <param name="key">缓存名称</param>
+        /// <param name="o">缓存内容</param>
+        /// <param name="second">缓存时间(秒)</param>
+        public void Add<T>(string key, T o, int second)
+        {
+            if (second > 0)
+            {
+                RedisManager.Instance.CacheClient.Set(key, o, DateTime.Now.AddSeconds(second));
+            }
+            else
+            {
+                RedisManager.Instance.CacheClient.Set(key, o);
+            }
+        }
+        
+        /// <summary>
+        /// 删除指定缓存
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(string key)
+        {
+            if (RedisManager.Instance.CacheClient.ContainsKey(key))
+                RedisManager.Instance.CacheClient.Remove(key);
+        }
+        
+        /// <summary>
+        /// 删除所有缓存
+        /// </summary>
+        public void RemoveAll()
+        {
+            RedisManager.Instance.CacheClient.FlushAll();
+        }
+
+        /// <summary>
+        /// 获得缓存数据
+        /// </summary>
+        /// <param name="objId"></param>
+        /// <returns></returns>
+        public object Get(string key)
+        {
+            return RedisManager.Instance.CacheClient.Get(key);
+        }
+        /// <summary>
+        /// 获得缓存数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public T Get<T>(string key)
+        {
+            return RedisManager.Instance.CacheClient.Get<T>(key);
+        }
+
+        /// <summary>
+        /// 判断此缓存是否有效
+        /// </summary>
+        /// <param name="objID"></param>
+        /// <returns></returns>
+        public bool isExists(string key)
+        {
+            return RedisManager.Instance.CacheClient.ContainsKey(key);
+        }
+        #endregion
+    }
+}
